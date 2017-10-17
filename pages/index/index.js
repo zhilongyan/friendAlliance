e08fd6183a26aee2e27df5c9273a422486c03aa7
yanzhilong <308434799@qq.com>
2017-10-17 14:00:16 +0800

    增加图片

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,14 +7,96 @@ Page({
     ],
     buttonGroup : [
       [
-        1,2,3,4,5
+        {
+          img : '/images/index/secKill.png',
+          text : '限时秒杀'
+        },
+        {
+          img: '/images/index/clearance.png',
+          text: '品牌清仓'
+        },
+        {
+          img: '/images/index/discount.png',
+          text: '名品折扣'
+        },
+        {
+          img: '/images/index/sea.png',
+          text: '海淘'
+        },
+        {
+          img: '/images/index/secKill.png',
+          text: '限时秒杀'
+        },
+        {
+          img: '/images/index/clearance.png',
+          text: '品牌清仓'
+        },
+        {
+          img: '/images/index/discount.png',
+          text: '名品折扣'
+        },
+        {
+          img: '/images/index/sea.png',
+          text: '海淘'
+        },
+        {
+          img: '/images/index/secKill.png',
+          text: '限时秒杀'
+        },
+        {
+          img: '/images/index/clearance.png',
+          text: '品牌清仓'
+        }
       ],
       [
-        1,2,4,45
+        {
+          img: '/images/index/sea2.png',
+          text: '限时秒杀'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '品牌清仓'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '名品折扣'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '海淘'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '限时秒杀'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '品牌清仓'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '名品折扣'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '海淘'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '限时秒杀'
+        },
+        {
+          img: '/images/index/sea2.png',
+          text: '品牌清仓'
+        }
       ]
-    ]
+    ],
+    buttonIndex : 0
   },
   onLoad : function(){
-
+  },
+  buttonChange : function(){
+    let index  = this.data.buttonIndex;
+    this.setData({buttonIndex : !index})
   }
 })
